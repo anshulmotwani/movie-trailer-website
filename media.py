5e34795addf760809514b821bd43099fae47c403
Anshul Motwani <anshulmotwani@live.com>
2015-06-15 17:32:13 +0530

    Update media.py

--- media.py
+++ media.py
@@ -3,15 +3,23 @@ import webbrowser
 
 # class named Movie which holds instance variables and instance functions
 # this class is used to make instance of each movie
+
+
 class Movie():
-	# this is a constructor which is called to make space in memory for a particular instance of clss
-	# it takes in attributes which are then allocated to instance variables
-	# self is used to point to current instance being allocated
-    def __init__(self, movie_title, movie_storyline, movie_poster_url, movie_trailer_url):
+    
+    # this is a constructor which is called to make space in memory
+    # for a particular instance of clss
+    # it takes in attributes which are then
+    # allocated to instance variables
+    #   self is used to point to current instance being allocated
+    
+    def __init__(self, title, storyline, poster_url, trailer_url):
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = movie_poster_url
         self.trailer_youtube_url = movie_trailer_url
     # this is a function used to open youtube video in web browser    
+    
     def show_trailer(self):
         webbrowser.open(self.youtube_trailer_url)
+        
